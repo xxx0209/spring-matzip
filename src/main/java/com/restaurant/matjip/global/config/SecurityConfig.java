eb9c06245da9c08993232e4002c35289dfbf48a4
xxx0209 <makelotus0209@gmail.com>
2026-01-28 18:29:46 +0900

    보안필터 기본 작성

--- src/main/java/com/restaurant/matjip/global/config/SecurityConfig.java
+++ src/main/java/com/restaurant/matjip/global/config/SecurityConfig.java
@@ -2,20 +2,72 @@ package com.restaurant.matjip.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * SecurityConfig 클래스
+ * - 모든 HTTP 요청에 대한 보안 필터를 적용
+ * - SecurityFilterChain 빈 하나가 Spring Security 필터 체인에 등록되어, 요청마다 실행
+ *
+ * 작성자: Shawn Lee
+ * 작성일: 2026-01-28
+ */
 @Configuration
 public class SecurityConfig {
 
+    //Spring Security 필터로, 요청마다 JWT를 확인하고 인증 정보를 SecurityContext에 세팅하는 역할
+    //private final JwtAuthenticationFilter jwtAuthFilter;
+
+    /**
+     * HttpSecurity는 웹 보안 설정을 정의할 때 사용하는 객체입니다.
+     * HTTP 요청이 들어왔을 때 어떤 요청이 인증/인가가 필요한지,
+     * 어떤 로그인/로그아웃 방식을 사용할지,
+     * CSRF, CORS, 세션 관리 등 웹 관련 보안 설정을 담당합니다.
+     *
+     * @param http 웹 보안 설정
+     * @return 설정한 필터체인
+     * @throws Exception 예외 발생 상황 설명
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // 모든 요청 허용
-                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
-                .formLogin(AbstractHttpConfigurer::disable); // 로그인 폼 비활성화
+        /*
+          http 설정
+          1. CORS(Cross-Origin Resource Sharing)
+             - 같은 출처(Scheme + Host + Port)에서만 리소스를 요청할 수 있음
+          2. CSRF(Cross-Site Request Forgery)
+             - CSRF는 웹 보안 공격 중 하나
+             - 공격자가 사용자가 로그인한 상태를 이용해 사용자 모르게 악의적 요청을 서버로 보내는 공격
+          3. SessionCreationPolicy.STATELESS
+             - 각 요청은 독립적이며, 인증 정보도 JWT, API 토큰 등으로 요청마다 전달해야 함
+          4. UsernamePasswordAuthenticationFilter
+             - Spring Security에서 기본 로그인 처리 필터
+             - 인증 성공 → SecurityContext에 인증 정보 저장
+        */
+        http.cors(Customizer.withDefaults()) //기본설정을 사용하되 @CrossOrigin 애노테이션 -> CorsConfigurationSource Bean이 있다면 그 설정 사용 없으면 기본값 적용(모든 요청 허용 X, 제한적)
+                .csrf(AbstractHttpConfigurer::disable)  //JWT는 stateless 인증이므로 CSRF 보호가 필요 없음 → 비활성화
+                //JWT 기반 인증은 세션을 사용하지 않음 → Stateless 서버는 인증 상태를 저장하지 않고, 매 요청마다 JWT 검증
+                .sessionManagement(sess ->
+                        sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(
+                                "/**",
+                                "/member/**",
+                                "/auth/**",
+                                "/api/**"
+                                ).permitAll() //인증 없이 접근 허용
+                            .anyRequest().authenticated() //그 외 모든 요청 → 인증 필요
+                );
+                //Spring Security의 기본 UsernamePasswordAuthenticationFilter 전에 jwtFilter를 실행
+                //즉, 요청이 들어오면 JWT 확인 후 SecurityContext에 인증 정보를 세팅하고 이후 필터 진행
+                //.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
+
 }
